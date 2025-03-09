63993bafa4cf19aa808592e861a56c987ad38f2c
uzairash <uzairashfaq90@gmail.com>
2025-03-09 22:48:08 +0500

    saasd

--- client/client.py
+++ client/client.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def func():
-    response = requests.get('http://localhost:5000/')
+    response = requests.get('http://api:5000/') # there will be localhost instead of contianer name in ECS fargate awsvpc
     return jsonify({'message': 'The API container responded with: ' + response.json()['message']})
 
 if __name__ == '__main__':
